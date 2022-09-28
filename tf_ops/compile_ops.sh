b83ee4c0ba043fbf2d225b0acd4af7f2cb7f5784
zhanghm1995 <zhanghm_1995@qq.com>
2022-09-28 10:27:46 +0800

    add .gitignore file

--- tf_ops/compile_ops.sh
+++ tf_ops/compile_ops.sh
@@ -2,7 +2,7 @@
 
 TF_CFLAGS=$(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))')
 TF_LFLAGS=$(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))')
-CUDA_ROOT=/usr/local/cuda-10.2
+CUDA_ROOT=/usr/local/cuda
 
 
 g++ -std=c++11 -shared ./interpolation/tf_interpolate.cpp -o ./interpolation/tf_interpolate_so.so  -I $CUDA_ROOT/include -lcudart -L $CUDA_ROOT/lib64/ -fPIC ${TF_CFLAGS} ${TF_LFLAGS} -O2
